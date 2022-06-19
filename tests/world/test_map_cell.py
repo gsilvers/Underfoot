ece38065e4e3fb6a9487a64b8a3a8c72c1deefae
Greg Silverstein <greg.silverstein@gmail.com>
2022-06-19 09:18:01 -0400

    small update try to fix CI for github

--- tests/world/test_map_cell.py
+++ tests/world/test_map_cell.py
@@ -4,6 +4,7 @@ from underfoot.world.map_cell import MapCell
 def test_create_map_cell() -> None:
     """
     Quick test ensures able to create map cell and retrieve its contents
+    These values are all fake not yet determined our full data model
     """
     test_cell = MapCell(
         cell_position=(1, 1, 1), cell_terrain='MTN1', cell_objects_list=['PLY01', 'NPC01']
