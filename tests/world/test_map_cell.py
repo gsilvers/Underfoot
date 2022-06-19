f7e36c6dc9f3c207a1ccd5b0c62eca71733ed7a7
Greg Silverstein <greg.silverstein@gmail.com>
2022-06-18 22:18:51 -0400

    fix tests

--- tests/world/test_map_cell.py
+++ tests/world/test_map_cell.py
@@ -1,4 +1,4 @@
-from underfoot.world import MapCell
+from underfoot.world.map_cell import MapCell
 
 
 def test_create_map_cell() -> None:
@@ -6,9 +6,9 @@ def test_create_map_cell() -> None:
     Quick test ensures able to create map cell and retrieve its contents
     """
     test_cell = MapCell(
-        cell_postition=(1, 1, 1), cell_terrain='MTN1', cell_objects=['PLY01', 'NPC01']
+        cell_position=(1, 1, 1), cell_terrain='MTN1', cell_objects_list=['PLY01', 'NPC01']
     )
     assert test_cell.cell_position == (1, 1, 1)
     assert test_cell.cell_terrain == 'MTN1'
-    assert test_cell.cell_objects == ['PLY01', 'NPC01']
+    assert test_cell.cell_objects_list == ['PLY01', 'NPC01']
     assert test_cell is not None
